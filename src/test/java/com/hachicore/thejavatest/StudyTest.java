f4a75712df6fdd252841852c3ef0bf36cfa84a40
8c6t <nkw8620@gmail.com>
2019-12-25 15:33:45 +0900

    1.7 JUnit 5 태깅과 필터링

--- src/test/java/com/hachicore/thejavatest/StudyTest.java
+++ src/test/java/com/hachicore/thejavatest/StudyTest.java
@@ -1,7 +1,6 @@
 package com.hachicore.thejavatest;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,37 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StudyTest {
 
     @Test
-    @DisplayName("스터디 만들기")
-    // @EnabledOnOs({ OS.WINDOWS, OS.MAC })
-    // @EnabledOnJre({ JRE.JAVA_8, JRE.JAVA_9, JRE.JAVA_10, JRE.JAVA_11 })
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "local")
+    @DisplayName("스터디 만들기 fast")
+    @Tag("fast")
     void create_new_study() {
-        String test_env = System.getenv("TEST_ENV");
-        // System.out.println(test_env);
-        // assumeTrue("LOCAL".equalsIgnoreCase(test_env));
-
-/*
-        assumingThat("LOCAL".equalsIgnoreCase(test_env), () -> {
-            Study actual = new Study(10);
-            assertThat(actual.getLimit()).isEqualTo(10);
-        });
-
-        assumingThat("hachicore".equalsIgnoreCase(test_env), () -> {
-            System.out.println("hachicore");
-            Study actual = new Study(10);
-            assertThat(actual.getLimit()).isGreaterThan(0);
-        });
-*/
-
         Study actual = new Study(10);
         assertThat(actual.getLimit()).isGreaterThan(0);
     }
 
     @Test
-    @DisplayName("스터디 다시 만들기")
-    // @DisabledOnOs(OS.MAC)
-    // @EnabledOnJre(JRE.OTHER)
-    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "hachicore")
+    @DisplayName("스터디 만들기 slow")
+    @Tag("slow")
     void create_new_study_again() {
         System.out.println("create1");
     }
