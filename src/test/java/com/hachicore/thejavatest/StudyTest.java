97feabe0a5e3cb870e0ee680098c4dab8167f065
8c6t <nkw8620@gmail.com>
2020-01-01 19:43:53 +0900

    1.14 JUnit 5 확장 모델

--- src/test/java/com/hachicore/thejavatest/StudyTest.java
+++ src/test/java/com/hachicore/thejavatest/StudyTest.java
@@ -2,6 +2,7 @@ package com.hachicore.thejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.AggregateWith;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
@@ -14,10 +15,15 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// @ExtendWith(FindSlowTestExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class StudyTest {
 
     int value = 1;
+
+    @RegisterExtension
+    static FindSlowTestExtension findSlowTestExtension =
+            new FindSlowTestExtension(1000L);
 
     @Order(2)
     @FastTest
@@ -33,7 +39,8 @@ class StudyTest {
     @SlowTest
     @DisplayName("스터디 만들기 slow")
     @Disabled
-    void create_new_study_again() {
+    void create_new_study_again() throws Exception {
+        Thread.sleep(1005L);
         System.out.println(this);
         System.out.println("create1 " + value++);
     }
