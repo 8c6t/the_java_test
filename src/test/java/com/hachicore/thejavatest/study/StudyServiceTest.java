a70092d57832f25d8dbffa3ee49a1876c49fcdb6
8c6t <nkw8620@gmail.com>
2020-02-13 19:16:31 +0900

    3.4 컨테이너 정보를 스프링 테스트에서 참조하기

--- src/test/java/com/hachicore/thejavatest/study/StudyServiceTest.java
+++ src/test/java/com/hachicore/thejavatest/study/StudyServiceTest.java
@@ -11,8 +11,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
@@ -30,11 +35,13 @@ import static org.mockito.Mockito.times;
 @ActiveProfiles("test")
 @Testcontainers
 @Slf4j
+@ContextConfiguration(initializers = StudyServiceTest.ContainerPropertyInitializer.class)
 class StudyServiceTest {
 
     @Mock MemberService memberService;
     @Autowired StudyRepository studyRepository;
-
+//    @Autowired Environment environment;
+    @Value("${container.port}") Integer port;
     @Container
     static GenericContainer postgreSQLContainer = new GenericContainer("postgres")
             .withExposedPorts(5432)
@@ -49,8 +56,9 @@ class StudyServiceTest {
     @BeforeEach
     void beforeEach() {
         System.out.println("==========");
-        System.out.println(postgreSQLContainer.getMappedPort(5432));
+//        System.out.println(environment.getProperty("container.port"));
 //        System.out.println(postgreSQLContainer.getLogs());
+        System.out.println(port);
         studyRepository.deleteAll();
     }
 
@@ -104,6 +112,14 @@ class StudyServiceTest {
         assertEquals(StudyStatus.OPENED, study.getStatus());
         assertNotNull(study.getOpenedDateTime());
         then(memberService).should().notify(study);
+    }
+
+    static class ContainerPropertyInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            TestPropertyValues.of("container.port=" + postgreSQLContainer.getMappedPort(5432))
+                .applyTo(applicationContext.getEnvironment());
+        }
     }
 
 }
