f5964ea60bfbdd1e7cf676934b21275bf4c5f11c
8c6t <nkw8620@gmail.com>
2020-02-21 20:53:00 +0900

    6.4 ArchUnit JUnit 5 연동

--- src/test/java/com/hachicore/thejavatest/study/ArchTests.java
+++ src/test/java/com/hachicore/thejavatest/study/ArchTests.java
@@ -1,47 +1,32 @@
 package com.hachicore.thejavatest.study;
 
-import com.tngtech.archunit.core.domain.JavaClasses;
-import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.hachicore.thejavatest.Application;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
+@AnalyzeClasses(packagesOf = Application.class)
 public class ArchTests {
 
-    @Test
-    void packageDependencyTests() {
-        JavaClasses classes = new ClassFileImporter().importPackages("com.hachicore.thejavatest");
+    @ArchTest
+    ArchRule domainPackageRule = classes().that().resideInAPackage("..domain..")
+            .should().onlyBeAccessed().byClassesThat()
+            .resideInAnyPackage("..study..", "..member..", "..domain..");
 
-        /**
-         * TODO ..domain.. 패키지에 있는 클래스는 ..study.., ..member.., ..domain 에서 참조 가능
-         * TODO ..member.. 패키지에 있는 클래스는 ..study..와 ..member.. 에서만 참조 가능
-         * TODO (반대로) ..domain.. 패키지는 ..member.. 패키지를 참조하지 못 한다.
-         * TODO ..study.. 패키지에 있는 클래스는 ..study.. 에서만 참조 가능
-         * TODO 순환 참조가 없어야 한다.
-         */
+    @ArchTest
+    ArchRule memberPackageRule = noClasses().that().resideInAPackage("..domain..")
+            .should().accessClassesThat().resideInAPackage("..member..");
 
-        ArchRule domainPackageRule = classes().that().resideInAPackage("..domain..")
-                .should().onlyBeAccessed().byClassesThat()
-                .resideInAnyPackage("..study..", "..member..", "..domain..");
-        domainPackageRule.check(classes);
+    @ArchTest
+    ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackage("..study..")
+            .should().accessClassesThat().resideInAPackage("..study..");
 
-
-        ArchRule memberPackageRule = noClasses().that().resideInAPackage("..domain..")
-                .should().accessClassesThat().resideInAPackage("..member..");
-        memberPackageRule.check(classes);
-
-
-        ArchRule studyPackageRule = noClasses().that().resideOutsideOfPackage("..study..")
-                .should().accessClassesThat().resideInAPackage("..study..");
-        studyPackageRule.check(classes);
-
-        ArchRule freeOfCycles = slices().matching("..thejavatest.(*)..")
-                .should().beFreeOfCycles();
-        freeOfCycles.check(classes);
-
-    }
+    @ArchTest
+    ArchRule freeOfCycles = slices().matching("..thejavatest.(*)..")
+            .should().beFreeOfCycles();
 
 }
